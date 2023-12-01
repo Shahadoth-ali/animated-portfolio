404d505bdfc36d35f9f052010818a0bff3baa143
Shahadoth-ali <md.sahadat.live@gmail.com>
2023-12-01 10:57:49 +0600

    fifth commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,6 +9,7 @@ const items=[
         title:"Air bnb clone",
         img:"https://images.pexels.com/photos/18704675/pexels-photo-18704675/free-photo-of-buildings-by-river-in-amasya-in-turkey.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
         decc:"Duo gubergren at et dolore sed tempor, stet elitr et diam elitr dolor et magna sadipscing ipsum, aliquyam kasd sea.",
+        demo:"https://booking-site-visit.vercel.app/"
     },
     {
         id:2,
@@ -41,6 +42,8 @@ const Single=({item})=>{
 
      const y=useTransform(scrollYProgress, [0,1], [-300,300]);
 
+
+
     return(
         <section>
            <div className="container">
@@ -53,7 +56,8 @@ const Single=({item})=>{
             <motion.div className="textContainer" style={{y}}>
                 <h2>{item.title}</h2>
                 <p>{item.decc}</p>
-                <button>See Demo</button>
+           <a href={item.demo}>
+            See Demo</a>
             </motion.div>
 
             </div>
