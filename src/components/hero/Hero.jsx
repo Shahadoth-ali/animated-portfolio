8b306035419270f261b85fb97d4b747ee8fdd9b6
Shahadoth-ali <md.sahadat.live@gmail.com>
2023-12-15 23:40:19 +0600

    final1 commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
 import "./hero.scss";
 import { motion } from "framer-motion";
 
+
 const textVariants={
     initial:{
         x:-500,
@@ -52,11 +53,11 @@ const Hero = () => {
             <motion.h2 variants={textVariants}>SHAHADOTH ALI</motion.h2>
             <motion.h1 variants={textVariants}>Web developer and UI designer</motion.h1>
             <motion.div variants={textVariants} className="buttons">
-               <motion.button
+            <motion.button
                 variants={textVariants}
                
                >
-                See the Latest Works</motion.button> 
+                See the Latest Works</motion.button>
 
                <motion.button variants={textVariants}>Contact Me</motion.button> 
             </motion.div>
